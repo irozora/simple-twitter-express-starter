ad3cd0e343df114c161825e535e1156888d0849c
Iro_Zora <moyashi1103@gmail.com>
2019-06-26 10:22:36 +0800

    Add model association

--- models/user.js
+++ models/user.js
@@ -12,6 +12,28 @@ module.exports = (sequelize, DataTypes) => {
     },
     {}
   )
-  User.associate = function(models) {}
+  User.associate = function(models) {
+    User.hasMany(models.Tweet)
+    User.hasMany(models.Reply)
+    User.belongsToMany(models.Tweet, {
+      through: models.Like,
+      foreignKey: 'UserId',
+      as: 'LikedTweets'
+    })
+
+    // 設定User的追蹤者關聯
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followingId',
+      as: 'Followers'
+    })
+
+    // 設定User正在追蹤的User關聯
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followerId',
+      as: 'Followings'
+    })
+  }
   return User
 }
