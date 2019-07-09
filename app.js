ca39e3936deb158e18fd16524ca9714bba4863e0
Iro_Zora <moyashi1103@gmail.com>
2019-07-09 11:54:49 +0800

    Changing Environment settings

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const methodOverride = require('method-override')
 const flash = require('connect-flash')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
